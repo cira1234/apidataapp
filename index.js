ad22c9ce4f306e2b691257d9bcbfd481a0792fd7
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-03-28 11:01:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,22 +16,22 @@ app.use(express.json())
 
 console.log('Connected to PlanetScale!')
 
-const path =require('path')
- const multer=require('multer');
- const storage=multer.diskStorage({
-    destination: (req,file,cb)=>{
-        cb(null,'image')
-    },
-    filename:(req,file,cb)=>{
-        console.log(file)
-        cb(null,Date.now()+path.extname(file.originalname))
-    }
- })
- const upload =multer({storage:storage}) 
+// const path =require('path')
+//  const multer=require('multer');
+//  const storage=multer.diskStorage({
+//     destination: (req,file,cb)=>{
+//         cb(null,'image')
+//     },
+//     filename:(req,file,cb)=>{
+//         console.log(file)
+//         cb(null,Date.now()+path.extname(file.originalname))
+//     }
+//  })
+//  const upload =multer({storage:storage}) 
 
-app.post("/upload",upload.single('images'),(req,res)=>{
-    res.send("Image upload complete");
-})
+// app.post("/upload",upload.single('images'),(req,res)=>{
+//     res.send("Image upload complete");
+// })
 
 
 
