d2da1e08dcdeadeaa63560154152f69530987119
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-03-28 09:54:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ app.post("/upload",upload.single('images'),(req,res)=>{
     res.send("Image upload complete");
 })
 
+
+
+
+
 app.get('/insertmenu/:name/:price/:detail/:typepush',function(req,res,next){
     // insertapi.push(req.body)
      //let json =req.body
@@ -65,7 +69,7 @@ app.get('/insertmenu/:name/:price/:detail/:typepush',function(req,res,next){
 
 
 
-app.get('/insertcart/:namemenu/:price/:number/:imgmenu/:typepush/:menuid',function(req,res,next){
+ app.get('/insertcart/:namemenu/:price/:number/:imgmenu/:typepush/:menuid',function(req,res,next){
     // insertapi.push(req.body)
      //let json =req.body
  
@@ -98,39 +102,6 @@ app.get('/insertcart/:namemenu/:price/:number/:imgmenu/:typepush/:menuid',functi
  })
 
 
-
-
-app.get('/insertapi/:name/:depart/:tel/:lv',function(req,res,next){
-   // insertapi.push(req.body)
-    //let json =req.body
-
-    var name=req.params.name;
-    var depart=req.params.depart;
-    var tel=req.params.tel;
-    var lv=req.params.lv;
-
-
-    connection.query(
-    'insert into employee (name,depart,tel,lv) values (?,?,?,?)',     
-    [name,depart,tel,lv],
-
-        console.log(name),
-        console.log(depart),
-        console.log(tel),
-        console.log(lv),
-        console.log(),
-
-
-        function(err,result){
-            res.json(result);
-            console.log(result);
-            //console.log(field);
-        }
-    )
-    
-})
-
-
 app.post('/insertdata',function(req,res,next){
     const id=req.params.id;
     connection.query(
@@ -147,93 +118,6 @@ app.post('/insertdata',function(req,res,next){
 })
 
 
-app.get('/getid',function(req,res,next){
-    const id=req.params.id;
-    connection.query(
-
-        'select * from menu where id =?',[id],
-        function(err,result){
-            res.json(result);
-            console.log(result);
-            console.log(field);
-        }
-    )
-    
-})
-
-
-app.get('/select',function(req,res,next){
-    connection.query(
-
-        'select * from employee',
-        function(err,result,field){
-            res.json(result);
-            console.log(result);
-            console.log(field);
-        }
-    )
-    
-})
-
-app.get('/selectmenuid/:id',function(req,res,next){
-       var id=req.params.id;
-    connection.query(
-
-        'select  *from menu where id=? ',[id],
-        function(err,result,field){
-            res.json(result);
-            console.log(result);
-            console.log(field);
-        }
-    )
-    
-})
-
-app.get('/selectdetailorder/:id',function(req,res,next){
-    var id=req.params.id;
- connection.query(
-
-     'select namemenu,price,(price*number) AS priceupdate,number,imgmenu,datail,table_id from priceup where id=? ',[id],
-     function(err,result,field){
-         res.json(result);
-         console.log(result);
-         console.log(field);
-     }
- )
- 
-})
-
-
-app.get('/selectcart/:id',function(req,res,next){
-    var id=req.params.id;
- connection.query(
-
-     'select id,namemenu,price,(price*number) AS priceupdate,number,imgmenu from priceup where table_id=? and checkorder=0',[id],
-     function(err,result,field){
-         res.json(result);
-         console.log(result);
-         console.log(field);
-     }
- )
- 
-})
-
-
-
-app.get('/selecteditcart/:id',function(req,res,next){
-    var id=req.params.id;
- connection.query(
-
-     'select id,namemenu,price,(price*number) AS priceupdate,number,imgmenu from priceup where id=? and checkorder=0',[id],
-     function(err,result,field){
-         res.json(result);
-         console.log(result);
-         console.log(field);
-     }
- )
- 
-})
-
 
 
 app.get('/selectmenu',function(req,res,next){
@@ -249,11 +133,10 @@ app.get('/selectmenu',function(req,res,next){
     
 })
 
-
-app.get('/pork.jpg',function(req,res,next){
+app.get('/selectmenuid/:id',function(req,res,next){
+    const id=req.params.id;
     connection.query(
-
-        'select * from menu',
+        'select * from menu where id=?',[id],
         function(err,result,field){
             res.json(result);
             console.log(result);
@@ -262,6 +145,52 @@ app.get('/pork.jpg',function(req,res,next){
     )
     
 })
+
+app.get('/selectcart/:id',function(req,res,next){
+    const id=req.params.id;
+    connection.query(
+
+        'select * from priceup where table_id=?',[id],
+        function(err,result,field){
+            res.json(result);
+            console.log(result);
+            console.log(field);
+        }
+    )
+    
+})
+
+
+app.get('/selectdetail/:id',function(req,res,next){
+    const id=req.params.id;
+    connection.query(
+
+        'select namemenu,price,number,(price*number) AS priceupdate,imgmenu,datail,table_id from priceup where id=?',[id],
+        function(err,result,field){
+            res.json(result);
+            console.log(result);
+            console.log(field);
+        }
+    )
+    
+})
+
+
+app.get('/selectorder',function(req,res,next){
+    connection.query(
+
+        'SELECT tablenumber.table_id,namemenu,price,(price*number) AS numberprice,imgmenu,ordermenu.id,ordermenu.number,ordertable_id,menu_id,orderstatus FROM `ordermenu` INNER JOIN menu ON menu_id = menu.id INNER JOIN tablenumber ON ordertable_id = table_id;',
+        function(err,result,field){
+            res.json(result);
+            console.log(result);
+            console.log(field);
+        }
+    )
+    
+})
+
+
+
 
 
 app.put('/updatedata',function(req,res,next){
@@ -278,62 +207,6 @@ app.put('/updatedata',function(req,res,next){
     )
     
 })
-
-
-
-
-app.get('/users/:id',function(req,res,next){
-    const id=req.params.id;
-    connection.query(
-
-        'select * from employee where id=?',[id],
-        function(err,result){
-            res.json(result);
-            //console.log(result);
-            //console.log(field);
-        }
-    )
-    
-})
-
-
-
-app.get('/insertmenu/:name/:price/:detail/:typepush',function(req,res,next){
-    // insertapi.push(req.body)
-     //let json =req.body
- 
-     var name=req.params.name;
-     var price=req.params.price;
-     var detail=req.params.detail;
-     var type=req.params.typepush;
-      var img=req.params.avatar
- 
-     connection.query(
-     'insert into menu (namemenu,price,imgmenu,typefood,menudetail) values (?,?,?,?,?)',     
-     [name,price,'',type,detail],
- 
-         console.log(name),
-         console.log(price),
-         console.log(detail),
-         console.log(type),
-         console.log(),
- 
- 
-         function(err,result){
-             res.json(result);
-             console.log(result);
-             //console.log(field);
-         }
-     )
-     
- })
-
-
-/*
-app.get('/products/:id',function(req,res,next){
-    res.json({msg:'api connected!'})
-})
-*/
 
 app.listen(3000,function(){
     console.log('enable port 3000')
