a9e45392fc92bc80298669943af0306ac7d56336
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-03-29 12:16:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ app.get('/selectcart/:id',function(req,res,next){
     const id=req.params.id;
     connection.query(
 
-        'select namemenu,price,(price*number) AS priceupdate,number,imgmenu,datail,menu_id from priceup where table_id=? and checkorder=0',[id],
+        'select id,namemenu,price,(price*number) AS priceupdate,number,imgmenu,datail,menu_id from priceup where table_id=? and checkorder=0',[id],
         function(err,result,field){
             res.json(result);
             console.log(result);
